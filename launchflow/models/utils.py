24cf3edcfe925bb3d0681227a92afdfe4ce05847
Caleb Van Dyke <caleb@launchflow.com>
2024-08-22 07:06:13 -0500

    chore: fix dict

--- launchflow/models/utils.py
+++ launchflow/models/utils.py
@@ -40,6 +40,59 @@ from launchflow.resource import Resource
 from launchflow.service import Service
 
 RESOURCE_PRODUCTS_TO_RESOURCES = {
+    ResourceProduct.UNKNOWN.value: Resource,
+    # GCP product types
+    ResourceProduct.GCP_SQL_POSTGRES.value: CloudSQLPostgres,
+    ResourceProduct.GCP_SQL_USER.value: CloudSQLUser,
+    ResourceProduct.GCP_ARTIFACT_REGISTRY_REPOSITORY.value: ArtifactRegistryRepository,
+    ResourceProduct.GCP_SQL_DATABASE.value: CloudSQLDatabase,
+    ResourceProduct.GCP_PUBSUB_TOPIC.value: PubsubTopic,
+    ResourceProduct.GCP_PUBSUB_SUBSCRIPTION.value: PubsubSubscription,
+    ResourceProduct.GCP_STORAGE_BUCKET.value: GCSBucket,
+    ResourceProduct.GCP_BIGQUERY_DATASET.value: BigQueryDataset,
+    ResourceProduct.GCP_MEMORYSTORE_REDIS.value: MemorystoreRedis,
+    # TODO consider having a separate resource product per compute engine type
+    ResourceProduct.GCP_COMPUTE_ENGINE.value: ComputeEngine,
+    ResourceProduct.GCP_SECRET_MANAGER_SECRET.value: SecretManagerSecret,
+    ResourceProduct.GCP_LAUNCHFLOW_CLOUD_RELEASER.value: GCPReleaser,
+    ResourceProduct.GCP_CLOUD_TASKS_QUEUE.value: CloudTasksQueue,
+    ResourceProduct.GCP_CLOUD_RUN_SERVICE_CONTAINER.value: CloudRunServiceContainer,
+    ResourceProduct.GCP_CUSTOM_DOMAIN_MAPPING.value: CustomDomainMapping,
+    ResourceProduct.GCP_WORKBENCH_INSTANCE.value: WorkbenchInstance,
+    ResourceProduct.GCP_REGIONAL_MANAGED_INSTANCE_GROUP.value: RegionalManagedInstanceGroup,
+    ResourceProduct.GCP_FIREWALL_ALLOW_RULE.value: FirewallAllowRule,
+    ResourceProduct.GCP_COMPUTE_HTTP_HEALTH_CHECK.value: HttpHealthCheck,
+    ResourceProduct.GCP_REGIONAL_AUTO_SCALER.value: RegionalAutoscaler,
+    # AWS product types
+    ResourceProduct.AWS_RDS_POSTGRES.value: RDSPostgres,
+    ResourceProduct.AWS_ELASTICACHE_REDIS.value: ElasticacheRedis,
+    # TODO consider having a separate resource product per EC2 instance type
+    ResourceProduct.AWS_EC2.value: EC2,
+    ResourceProduct.AWS_ALB.value: ApplicationLoadBalancer,
+    ResourceProduct.AWS_ACM_CERTIFICATE.value: ACMCertificate,
+    ResourceProduct.AWS_S3_BUCKET.value: S3Bucket,
+    ResourceProduct.AWS_SECRETS_MANAGER_SECRET.value: SecretsManagerSecret,
+    ResourceProduct.AWS_CODEBUILD_PROJECT.value: CodeBuildProject,
+    ResourceProduct.AWS_ECR_REPOSITORY.value: ECRRepository,
+    ResourceProduct.AWS_ECS_FARGATE_SERVICE_CONTAINER.value: ECSFargateServiceContainer,
+    ResourceProduct.AWS_ECS_CLUSTER.value: ECSCluster,
+    ResourceProduct.AWS_SQS_QUEUE.value: SQSQueue,
+    ResourceProduct.AWS_LAUNCHFLOW_CLOUD_RELEASER.value: AWSReleaser,
+    # Local product types
+    # TODO consider having a separate resource product for each local docker type
+    ResourceProduct.LOCAL_DOCKER.value: DockerResource,
+}
+
+
+SERVICE_PRODUCTS_TO_SERVICES = {
+    ServiceProduct.UNKNOWN.value: Service,
+    # AWS product types
+    ServiceProduct.AWS_ECS_FARGATE.value: ECSFargate,
+    # GCP product types
+    ServiceProduct.GCP_CLOUD_RUN.value: CloudRun,
+    ServiceProduct.GCP_COMPUTE_ENGINE.value: ComputeEngineService,
+}
+RESOURCE_PRODUCTS_TO_RESOURCES = {
     ResourceProduct.UNKNOWN: Resource,
     # GCP product types
     ResourceProduct.GCP_SQL_POSTGRES: CloudSQLPostgres,
