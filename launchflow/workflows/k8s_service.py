da580df0d77e22dee23fc4e80e702306d2e5d794
Caleb Van Dyke <caleb@launchflow.com>
2024-08-27 10:55:42 -0500

    add some more tests

--- launchflow/workflows/k8s_service.py
+++ launchflow/workflows/k8s_service.py
@@ -4,82 +4,6 @@ import asyncio
 import time
 
 
-async def _wait_for_ingress_ip(
-    core_client: client.CoreV1Api,
-    service_name: str,
-    namespace: str,
-    timeout: int = 300,
-) -> str:
-    start_time = time.time()
-    while True:
-        service = core_client.read_namespaced_service(
-            name=service_name, namespace=namespace
-        )
-        if service.status.load_balancer.ingress:
-            return service.status.load_balancer.ingress[0].ip
-
-        if time.time() - start_time > timeout:
-            raise TimeoutError(
-                f"Timeout waiting for Ingress {service_name} to get an IP address"
-            )
-
-        await asyncio.sleep(5)
-
-
-# TODO: make this async
-async def deploy_new_k8s_service(
-    k8_client: client.AppsV1Api,
-    node_pool_id: str,
-    docker_image: str,
-    service_name: str,
-    port: int,
-    namespace: str,
-):
-    container = client.V1Container(
-        name=service_name,
-        image=docker_image,
-        ports=[client.V1ContainerPort(container_port=port)],
-    )
-    template = client.V1PodTemplateSpec(
-        metadata=client.V1ObjectMeta(labels={"app": service_name}),
-        spec=client.V1PodSpec(
-            containers=[container],
-            node_selector={"nodepool": node_pool_id},
-        ),
-    )
-    spec = client.V1DeploymentSpec(
-        replicas=1,
-        template=template,
-        selector={"matchLabels": {"app": service_name}},
-    )
-    deployment = client.V1Deployment(
-        api_version="apps/v1",
-        kind="Deployment",
-        metadata=client.V1ObjectMeta(name=service_name),
-        spec=spec,
-    )
-    k8_client.create_namespaced_deployment(namespace=namespace, body=deployment)
-
-    service = client.V1Service(
-        api_version="v1",
-        kind="Service",
-        metadata=client.V1ObjectMeta(name=service_name),
-        spec=client.V1ServiceSpec(
-            selector={"app": service_name},
-            # TODO: should add options to expose this differently
-            ports=[client.V1ServicePort(protocol="TCP", port=port, target_port=port)],
-            type="LoadBalancer",
-        ),
-    )
-
-    core_client = client.CoreV1Api()
-    core_client.create_namespaced_service(namespace=namespace, body=service)  # type: ignore
-
-    ip = await _wait_for_ingress_ip(core_client, service_name, namespace)
-
-    return f"http://{ip}"
-
-
 async def update_k8s_service(
     docker_image: str,
     service_name: str,
@@ -107,9 +31,7 @@ async def update_k8s_service(
     template_spec.service_account_name = k8_service_account
 
     env_vars = [
-        client.V1EnvVar(
-            name="LAUNCHFLOW_ARTIFACT_BUCKET", value=f"gs://{artifact_bucket}"
-        ),
+        client.V1EnvVar(name="LAUNCHFLOW_ARTIFACT_BUCKET", value=artifact_bucket),
         client.V1EnvVar(name="LAUNCHFLOW_PROJECT", value=launchflow_project),
         client.V1EnvVar(name="LAUNCHFLOW_ENVIRONMENT", value=launchflow_environment),
         client.V1EnvVar(name="LAUNCHFLOW_CLOUD_PROVIDER", value=cloud_provider),
---
+++ tests/workflows/k8_service_test.py
@@ -0,0 +1,85 @@
+import unittest
+from unittest import mock
+
+from launchflow.workflows.k8s_service import update_k8s_service
+from kubernetes import client
+
+
+class K8ServiceTest(unittest.IsolatedAsyncioTestCase):
+    @mock.patch("launchflow.workflows.k8s_service.client")
+    async def test_update_k8s_service(self, k8_client_mock: mock.MagicMock):
+        # Setup inputs
+        docker_image = "gcr.io/launchflow-test/test:latest"
+        service_name = "service"
+        namespace = "namespace"
+        artifact_bucket = "gs://launchflow-test-artifacts"
+        launchflow_project = "launchflow-test"
+        launchflow_environment = "staging"
+        k8_service_account = "service-account"
+        cloud_provider = "gcp"
+        deployment_id = "deployment_id"
+        environment_vars = {"A": "B"}
+
+        # Setup mocks
+        k8_client_mock.V1EnvVar = client.V1EnvVar
+        apps_client = mock.MagicMock()
+        k8_client_mock.AppsV1Api.return_value = apps_client
+
+        mock_deployment = mock.MagicMock()
+        apps_client.read_namespaced_deployment.return_value = mock_deployment
+
+        mock_deployment_status = mock.MagicMock()
+        mock_deployment_status.status.updated_replicas = 1
+        mock_deployment_status.status.replicas = 1
+        mock_deployment_status.status.available_replicas = 1
+        mock_deployment_status.spec.replicas = 1
+        apps_client.read_namespaced_deployment_status.return_value = (
+            mock_deployment_status
+        )
+
+        _ = await update_k8s_service(
+            docker_image=docker_image,
+            service_name=service_name,
+            namespace=namespace,
+            artifact_bucket=artifact_bucket,
+            launchflow_project=launchflow_project,
+            launchflow_environment=launchflow_environment,
+            k8_service_account=k8_service_account,
+            cloud_provider=cloud_provider,
+            deployment_id=deployment_id,
+            environment_vars=environment_vars,
+        )
+
+        apps_client.read_namespaced_deployment.assert_called_once_with(
+            name=service_name, namespace=namespace
+        )
+        apps_client.patch_namespaced_deployment.assert_called_once_with(
+            name=service_name, namespace=namespace, body=mock_deployment
+        )
+        apps_client.read_namespaced_deployment_status.assert_called_once_with(
+            name=service_name, namespace=namespace
+        )
+        # Verify that the container spec was updated appropriately
+        self.assertEqual(
+            mock_deployment.spec.template.spec.containers[0].image, docker_image
+        )
+        self.assertEqual(
+            mock_deployment.spec.template.spec.containers[0].env,
+            [
+                client.V1EnvVar(
+                    name="LAUNCHFLOW_ARTIFACT_BUCKET",
+                    value=artifact_bucket,
+                ),
+                client.V1EnvVar(name="LAUNCHFLOW_PROJECT", value=launchflow_project),
+                client.V1EnvVar(
+                    name="LAUNCHFLOW_ENVIRONMENT", value=launchflow_environment
+                ),
+                client.V1EnvVar(name="LAUNCHFLOW_CLOUD_PROVIDER", value=cloud_provider),
+                client.V1EnvVar(name="LAUNCHFLOW_DEPLOYMENT_ID", value=deployment_id),
+                client.V1EnvVar(name="A", value="B"),
+            ],
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
